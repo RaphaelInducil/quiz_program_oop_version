f934f2e831034e9b8eb49f10a1c11f642188cd7d
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-05-22 21:07:27 +0800

    pseudocodeeee

---
+++ quiz_program_oop.py
@@ -0,0 +1,15 @@
+# raphael clouiee inducil
+# BSCpE 1-2 
+# 04-07-25
+# assignment 11: Convert assignment 9 and 10 to OOP
+# Turn assignment 9 (quiz_maker) and 10 (quiz_program) into OOP
+
+# create class quiz_maker.py oop vers.
+# create class quiz_program.py oop vers.
+# import both projects
+# create menu to run the quiz maker and quiz program
+    # while loop
+    # display menu
+    # get user input
+    # elif condition for choices
+# end
